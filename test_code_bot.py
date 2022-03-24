fb2f691f3f1edeaddda91bb6a42b02b663889ae5
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-03-23 22:28:46 -0400

    Fixed up the test file

--- test_code_bot.py
+++ test_code_bot.py
@@ -1,11 +1,13 @@
 import json
+import os
+from urllib.request import urlopen
 
 from dotenv import load_dotenv
 
-from code_bot_utils import *
+import code_bot_utils as utils
 
 queries = json.load(open("queries.json"))
-keyword_mapping = generate_keyword_mapping(queries)
+keyword_mapping = utils.generate_keyword_mapping(queries)
 
 
 def top_queries(top_ids):
@@ -13,22 +15,28 @@ def top_queries(top_ids):
 
 
 def test_search_exact_match_first():
-    top_ids = search(keyword_mapping, [
-                     "what", "is", "a", "magic", "number"])[:1]
+    top_ids = utils.search(
+        keyword_mapping,
+        ["what", "is", "a", "magic", "number"]
+    )[:1]
     assert 1 in top_ids
     assert "What is a magic number?" in top_queries(top_ids)
 
 
 def test_search_exact_match_middle():
-    top_ids = search(keyword_mapping, [
-                     "what", "is", "method", "overriding"])[:1]
+    top_ids = utils.search(
+        keyword_mapping,
+        ["what", "is", "method", "overriding"]
+    )[:1]
     assert 12 in top_ids
     assert "What is method overriding?" in top_queries(top_ids)
 
 
 def test_search_routine_match():
-    top_ids = search(keyword_mapping, [
-                     "what", "does", "implements", "mean"])[:3]
+    top_ids = utils.search(
+        keyword_mapping,
+        ["what", "does", "implements", "mean"]
+    )[:3]
     assert 14 in top_ids
     assert "What is the implements relationship?" in top_queries(top_ids)
 
@@ -43,18 +51,18 @@ def test_generate_keyword_mapping():
         "cow": {0: 11},
         "now": {0: 10}
     }
-    keyword_mapping = generate_keyword_mapping([test_query])
+    keyword_mapping = utils.generate_keyword_mapping([test_query])
     assert keyword_mapping == expected_mapping
 
 
 def test_generate_similar_queries():
-    generate_similar_queries(queries, keyword_mapping)
+    utils.generate_similar_queries(queries, keyword_mapping)
     assert 1 not in queries[1]["similar_queries"]
 
 
 def test_load_knowledge_type_0():
     load_dotenv(dotenv_path=".env.test_type_0")
-    actualType, actualKnowledge = load_knowledge()
+    actualType, actualKnowledge = utils.load_knowledge()
     expectedType = 0
     data = urlopen("https://raw.githubusercontent.com/TheRenegadeCoder/cs-query-bot/main/queries.json") \
         .read() \
@@ -67,7 +75,7 @@ def test_load_knowledge_type_0():
 
 def test_load_knowledge_type_1():
     load_dotenv(dotenv_path=".env.test_type_1")
-    actualType, actualKnowledge = load_knowledge()
+    actualType, actualKnowledge = utils.load_knowledge()
     expectedType = 1
     expectedKnowledge = json.load(open("queries.json"))
     os.environ.pop("KNOWLEDGE_PATH")
@@ -76,7 +84,7 @@ def test_load_knowledge_type_1():
 
 
 def test_load_knowledge_type_2():
-    actualType, actualKnowledge = load_knowledge()
+    actualType, actualKnowledge = utils.load_knowledge()
     expectedType = 2
     expectedKnowledge = json.load(open("queries.json"))
     assert expectedType == actualType
