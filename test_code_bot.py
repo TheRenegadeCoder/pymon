4282e2f8175fb3a174b5137f21a96f5c2f3fccfd
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-03-23 22:09:33 -0400

    Cleaned up test file

--- test_code_bot.py
+++ test_code_bot.py
@@ -1,27 +1,37 @@
 import json
-from code_bot_utils import *
+
 from dotenv import load_dotenv
+
+from code_bot_utils import *
 
 queries = json.load(open("queries.json"))
 keyword_mapping = generate_keyword_mapping(queries)
 
+
 def top_queries(top_ids):
     return [queries[i]["query"] for i in top_ids]
 
+
 def test_search_exact_match_first():
-    top_ids = search(keyword_mapping, ["what", "is", "a", "magic", "number"])[:1]
+    top_ids = search(keyword_mapping, [
+                     "what", "is", "a", "magic", "number"])[:1]
     assert 1 in top_ids
     assert "What is a magic number?" in top_queries(top_ids)
 
+
 def test_search_exact_match_middle():
-    top_ids = search(keyword_mapping, ["what", "is", "method", "overriding"])[:1]
+    top_ids = search(keyword_mapping, [
+                     "what", "is", "method", "overriding"])[:1]
     assert 12 in top_ids
     assert "What is method overriding?" in top_queries(top_ids)
 
+
 def test_search_routine_match():
-    top_ids = search(keyword_mapping, ["what", "does", "implements", "mean"])[:3]
+    top_ids = search(keyword_mapping, [
+                     "what", "does", "implements", "mean"])[:3]
     assert 14 in top_ids
     assert "What is the implements relationship?" in top_queries(top_ids)
+
 
 def test_generate_keyword_mapping():
     test_query = {
@@ -29,16 +39,18 @@ def test_generate_keyword_mapping():
         "response": "The cow is brown."
     }
     expected_mapping = {
-        "brown": {0: 11}, # index: weight
+        "brown": {0: 11},  # index: weight
         "cow": {0: 11},
         "now": {0: 10}
     }
     keyword_mapping = generate_keyword_mapping([test_query])
     assert keyword_mapping == expected_mapping
 
+
 def test_generate_similar_queries():
     generate_similar_queries(queries, keyword_mapping)
-    assert 1 not in queries[1]["similar_queries"] 
+    assert 1 not in queries[1]["similar_queries"]
+
 
 def test_load_knowledge_type_0():
     load_dotenv(dotenv_path=".env.test_type_0")
@@ -52,6 +64,7 @@ def test_load_knowledge_type_0():
     assert expectedType == actualType
     assert expectedKnowledge == actualKnowledge
 
+
 def test_load_knowledge_type_1():
     load_dotenv(dotenv_path=".env.test_type_1")
     actualType, actualKnowledge = load_knowledge()
@@ -60,6 +73,7 @@ def test_load_knowledge_type_1():
     os.environ.pop("KNOWLEDGE_PATH")
     assert expectedType == actualType
     assert expectedKnowledge == actualKnowledge
+
 
 def test_load_knowledge_type_2():
     actualType, actualKnowledge = load_knowledge()
